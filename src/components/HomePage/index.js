1aec4e07bc7309ecaf0486d9b34298ecc735eb91
boraajay <165030977+boraajay@users.noreply.github.com>
2024-11-26 09:24:23 +0530

    Update index.js

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,11 +1,10 @@
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';  // Import for rendering RichText
-import styles from '../../styles/Home.css'
 
 const Home = ({homePage}) =>{
   return(  
         <div>
-            <h1 id='homePageTitle' className={styles.title}>{homePage?.fields?.homePageTitle}</h1>
-            <div id='homePagedescription' className={styles.description}>
+            <h1 id='homePageTitle' className="title">{homePage?.fields?.homePageTitle}</h1>
+            <div id='homePagedescription' className="description">
                 {documentToReactComponents(homePage?.fields?.homePageDescription)}
             </div>
         </div>)
