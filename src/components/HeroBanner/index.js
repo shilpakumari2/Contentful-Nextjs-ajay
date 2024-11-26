dd31caaef2ae4a4982ecdfb8e5af1cb7d6bf2d69
boraajay <165030977+boraajay@users.noreply.github.com>
2024-11-26 09:23:30 +0530

    Update index.js

--- src/components/HeroBanner/index.js
+++ src/components/HeroBanner/index.js
@@ -1,11 +1,10 @@
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';  // Import for rendering RichText
-import styles from '../../styles/Home.css'
 
 const Hero = ({heroBanner}) =>{
   return(  
         <div>
-            <h1 id='homePageTitle' className={styles.title}>{heroBanner?.fields?.title}</h1>
-            <div id='homePagedescription' className={styles.description}>
+            <h1 id='homePageTitle' className="title">{heroBanner?.fields?.title}</h1>
+            <div id='homePagedescription' className="description">
                 {documentToReactComponents(heroBanner?.fields?.heroDescription)}
             </div>
         </div>)
