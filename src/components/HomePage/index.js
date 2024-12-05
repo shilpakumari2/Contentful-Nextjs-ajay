d63c060406eeed80ede725a83ad56d7a9d8427f6
shilpa_kumari2 <shilpa.kumari2@concentrix.com>
2024-12-05 21:32:53 +0530

    Updated inspector props

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,22 +1,27 @@
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';  // Import for rendering RichText
-import ContentfulPreviewProvider from '../components/PreviewClient'
+import {
+    useContentfulInspectorMode,
+    useContentfulLiveUpdates,
+  } from '@contentful/live-preview/react';
+  import { ContentfulLivePreview } from '@contentful/live-preview';
 
 const Home = ({homePage}) =>{
+    const updatedBlog = useContentfulLiveUpdates(homePage);
+  const inspectorProps = useContentfulInspectorMode({ entryId: homePage.sys.id });
   return(  
-    <ContentfulPreviewProvider
-      locale="en-US" // This is required and allows you to set the locale once and have it reused throughout the preview
-      enableInspectorMode // This allows you to toggle the inspector mode which is on by default
-      enableLiveUpdates // This allows you to toggle the live updates which is on by default
-      debugMode // This allows you to toggle the debug mode which is off by default
-      targetOrigin="https://app.contentful.com" // This allows you to configure the allowed host of the live preview (default: ['https://app.contentful.com', 'https://app.eu.contentful.com'])
-    > 
+    
         <div>
-            <h1 id='homePageTitle' className="title">{homePage?.fields?.homePageTitle}</h1>
-            <div id='homePagedescription' className="description">
+            <h1 id='homePageTitle' className="title"
+                          {...inspectorProps({ fieldId: 'homePageTitle' })}>
+                                          {updatedBlog?.fields?.homePageTitle}
+
+                            </h1>
+            <div id='homePagedescription' className="description" {...inspectorProps({ fieldId: 'homePagedescription' })}>
+
                 {documentToReactComponents(homePage?.fields?.homePageDescription)}
+
             </div>
         </div>
-        </ContentfulPreviewProvider>
 
         )
 };
