323938f7999279fa14f6df63ccf202fd3097e61c
shilpa_kumari2 <shilpa.kumari2@concentrix.com>
2024-12-05 15:50:34 +0530

    Added preview

---
+++ src/components/PrivewClient.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { ContentfulLivePreviewInitConfig } from '@contentful/live-preview';
+import { ContentfulLivePreviewProvider } from '@contentful/live-preview/react';
+import React from 'react';
+import { PropsWithChildren } from 'react';
+
+export default function ContentfulPreviewProvider({
+  children,
+  ...props
+}: PropsWithChildren<ContentfulLivePreviewInitConfig>) {
+  return <ContentfulLivePreviewProvider {...props}>{children}</ContentfulLivePreviewProvider>;
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,22 @@
 import Home from '@/components/HomePage';
 import Hero from '../components/HeroBanner'; 
 import client from '@/lib/contentful'; 
+import ContentfulPreviewProvider from '../components/PreviewClient'
 
 const Main = ({ homePage, heroBanner }) => {
   return (
+    <ContentfulPreviewProvider
+      locale="en-US" // This is required and allows you to set the locale once and have it reused throughout the preview
+      enableInspectorMode // This allows you to toggle the inspector mode which is on by default
+      enableLiveUpdates // This allows you to toggle the live updates which is on by default
+      debugMode // This allows you to toggle the debug mode which is off by default
+      targetOrigin="https://app.contentful.com" // This allows you to configure the allowed host of the live preview (default: ['https://app.contentful.com', 'https://app.eu.contentful.com'])
+    > 
     <div className="container">
       <Home homePage={homePage} />
       <Hero heroBanner={heroBanner} />
     </div>
+    </ContentfulPreviewProvider>
   );
 };
 
